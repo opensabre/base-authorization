7b2b6098698561a31b039ec3698279556182f0ec
周涛 <zhoutaoo@foxmail.com>
2022-10-30 18:31:27 +0800

    1.引入依赖父包 2.加入starter依赖

---
+++ src/test/java/io/github/opensabre/authorization/DbTest.java
@@ -0,0 +1,14 @@
+package io.github.opensabre.organization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbTest {
+
+    @Test
+    void init() {
+
+    }
+
+}
