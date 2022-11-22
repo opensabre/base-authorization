f92c42150c8b29f9a4fbc2e0f4a4571a15e66d8d
周涛 <zhoutaoo@foxmail.com>
2022-11-22 23:15:03 +0800

    添加全局异常拦截

--- src/main/java/io/github/opensabre/authorization/exception/GlobalExceptionHandlerAdvice.java
+++ src/main/java/io/github/opensabre/authorization/exception/GlobalExceptionHandlerAdvice.java
@@ -1,11 +1,19 @@
 package io.github.opensabre.authorization.exception;
 
+import io.github.opensabre.common.core.entity.vo.Result;
 import io.github.opensabre.common.web.exception.DefaultGlobalExceptionHandlerAdvice;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandlerAdvice extends DefaultGlobalExceptionHandlerAdvice {
 
+    @ExceptionHandler(value = {InternalAuthenticationServiceException.class})
+    public Result internalAuthenticationServiceException(InternalAuthenticationServiceException ex) {
+        log.error("load user by username exception:{}", ex.getMessage());
+        return Result.fail(AuthErrorType.UNAUTHORIZED_CLIENT);
+    }
 }
