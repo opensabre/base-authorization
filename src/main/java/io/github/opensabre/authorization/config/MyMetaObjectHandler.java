46a14bda7087ce129d63260f9b0f3aca2e7f0eb2
周涛 <zhoutaoo@foxmail.com>
2022-11-30 21:16:02 +0800

    增加mybatis-plus配置

---
+++ src/main/java/io/github/opensabre/authorization/config/MyMetaObjectHandler.java
@@ -0,0 +1,9 @@
+package io.github.opensabre.authorization.config;
+
+import io.github.opensabre.common.web.handler.PoMetaObjectHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMetaObjectHandler extends PoMetaObjectHandler {
+
+}
